cca72adcb0540a85512bb5f09c2997ba07bdc6af
Adrien Vindreau <adrienvindreau@hotmail.fr>
2025-05-21 11:40:43 +0200

    maj modale

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,6 +1,8 @@
-let modal = null;
+let modal = null; //par défaut modal fermé
 
-const openModal = function (e) {
+/********* fonction afficher modal ************/
+
+const openModal = function (e) {  
     e.preventDefault();
     
     modal = document.querySelector(e.target.getAttribute("href"));
@@ -11,8 +13,10 @@ const openModal = function (e) {
     modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
     modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
     galleryModal();
-
+    switchGalerieFormulaire();
 }
+
+/********* fonction fermer modal ************/
 
 const closeModal = function (e) {
     if (modal === null) {
@@ -26,12 +30,15 @@ const closeModal = function (e) {
     modal.querySelector('.js-close-modal').removeEventListener('click', closeModal);
     modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
     modal = null;
-
 }
+
+/********* fonction afficher modal ************/
 
 const stopPropagation = function (e) {
     e.stopPropagation();
 }
+
+/********* fonction afficher modal ************/
 
 async function galleryModal() {
     const reponse = await fetch("http://localhost:5678/api/works");
@@ -42,7 +49,6 @@ async function galleryModal() {
     sectionGallery.innerHTML = '';
 
         oeuvres.forEach(oeuvre => {
-        console.log(oeuvre.title);
 
         const oeuvreElement = document.createElement("figure");
 
@@ -50,18 +56,46 @@ async function galleryModal() {
         imageOeuvre.src = oeuvre.imageUrl;
         imageOeuvre.alt = oeuvre.title;
 
-        const titreOeuvre = document.createElement("figcaption");
-        
+        /*const span 
+        const i*/
 
-        oeuvreElement.classList.add("category-" + oeuvre.category.id);
+        const boutonSupprimer = document.createElement("button");
+        boutonSupprimer.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+        boutonSupprimer.classList.add("btn-supprimer");
+
+        oeuvreElement.classList.add("id-" + oeuvre.id);
 
         sectionGallery.appendChild(oeuvreElement);
-
         oeuvreElement.appendChild(imageOeuvre);
+        oeuvreElement.appendChild(boutonSupprimer);
         
     });
 }
 
+/********* fonction passage formulaire / galerie  ************/
+
+function switchGalerieFormulaire() {
+    const boutonAjoutPhoto = document.querySelector(".ajout-photo");
+    const galerie = document.querySelector(".modal-galerie");
+    const formulaire = document.querySelector(".modal-formulaire");
+
+    boutonAjoutPhoto.addEventListener("click", () => {
+        galerie.style.display = "none";
+        formulaire.style.display = "flex";
+    });
+
+    const boutonRetour = document.querySelector(".js-return-modal");
+    if(boutonRetour) {
+        boutonRetour.addEventListener("click", (e) => {
+            e.stopPropagation();
+            formulaire.style.display = "none";
+            galerie.style.display = "flex";
+        });
+    }
+}
+
+
+/********* ecoute du click sur la modale  ************/
 document.querySelectorAll(".js-modal").forEach(a => {
     a.addEventListener('click', openModal);
 })
